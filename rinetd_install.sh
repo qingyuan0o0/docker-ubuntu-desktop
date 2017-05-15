fc2c34cbcd323b911106db9755d652b6399f2016
WinstonH <bjdhjj@126.com>
2017-05-15 17:07:13 +0800

    Update rinetd_install.sh

--- rinetd_install.sh
+++ rinetd_install.sh
@@ -6,4 +6,4 @@ cd rinetd
 mkdir -p /usr/man/man8  
 make && make install
 echo "rinetd was installed successfully! Now please edit /etc/rinetd.conf"
-echo "example 0.0.0.0 8080 192.168.1.1 8080"
+echo "# example 0.0.0.0 8080 127.0.0.1 8080" >> /etc/rinetd.conf
