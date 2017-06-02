2fccad47196598b01f1e205c7cc8df323b234bce
WinstonH <bjdhjj@126.com>
2017-06-03 02:25:42 +0800

    Create vnc.sh

---
+++ vnc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+while [ 1 ]
+do
+  ps -fe|grep firefox |grep -v grep
+  if [ $? -ne 0 ]
+  then
+  echo "start vnc ..."
+  rm /tmp/.X1-lock
+  rm /tmp/.X11-unix/X1
+  /usr/bin/vncserver :1 -geometry 1280x800 -depth 24
+  else
+  echo "vnc is running"
+  fi
+  sleep 30
+ done
