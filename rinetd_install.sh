34bc42481a37369ad1f0cb975795e7e634190220
WinstonH <bjdhjj@126.com>
2017-07-28 09:32:51 +0800

    Delete rinetd_install.sh

--- rinetd_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash   
-wget http://www.boutell.com/rinetd/http/rinetd.tar.gz  
-tar zxvf rinetd.tar.gz
-touch /etc/rinetd.conf
-cd rinetd  
-mkdir -p /usr/man/man8  
-make && make install
-echo "rinetd was installed successfully! Now please edit /etc/rinetd.conf"
-echo "# example 0.0.0.0 8080 127.0.0.1 8080" >> /etc/rinetd.conf
