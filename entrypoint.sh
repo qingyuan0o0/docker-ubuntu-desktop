6c32f7d949fcf3a2f17eb8954eb7f992f47d3f6d
WinstonH <bjdhjj@gmail.com>
2017-10-19 00:32:43 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,7 @@ if [ "$ETH"  ==  '0' ] ; then
     nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
 fi
 
-/etc/init.d/ssh restart
-echo "nameserver 123.207.137.88" > /etc/resolv.conf
-echo "nameserver 115.159.220.214" >> /etc/resolv.conf
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 /usr/bin/supervisord -c /etc/supervisord.conf
 exec "$@"
