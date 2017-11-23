92bba5e017dc6747c0e6f813b0390c36f9e0e1ac
WinstonH <bjdhjj@gmail.com>
2017-11-23 11:07:20 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,8 @@ if [ "$ETH"  ==  '0' ] ; then
 fi
 ln -s /noVNC/vnc.html /noVNC/index.html
 /etc/init.d/ssh restart
+
+# export the $PORT env
+sed -i "s/6080/$PORT/g" /etc/supervisord.conf
 /usr/bin/supervisord -c /etc/supervisord.conf
 exec "$@"
