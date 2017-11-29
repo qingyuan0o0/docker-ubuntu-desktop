296dcd9cf4164b23f08be7d148224947fa5138b5
WinstonH <bjdhjj@gmail.com>
2017-11-29 09:49:46 +0800

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 while [ 1 ]
 do
-  ps -fe|grep firefox |grep -v grep
+  ps -fe|grep Xtightvnc |grep -v grep
   if [ $? -ne 0 ]
   then
   echo "start vnc ..."
-  rm /tmp/.X1-lock
-  rm /tmp/.X11-unix/X1
+  date
   /usr/bin/vncserver :1 -geometry 1280x800 -depth 24
   else
   echo "vnc is running"
